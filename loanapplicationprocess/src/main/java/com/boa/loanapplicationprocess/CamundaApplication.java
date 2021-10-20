b473e36e82ab9fbbe5df921d09224f718b041aaf
eswaribala <parameswaribala@gmail.com>
2021-10-20 16:43:46 +0530

    updated

--- loanapplicationprocess/src/main/java/com/boa/loanapplicationprocess/CamundaApplication.java
+++ loanapplicationprocess/src/main/java/com/boa/loanapplicationprocess/CamundaApplication.java
@@ -6,6 +6,7 @@ import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
